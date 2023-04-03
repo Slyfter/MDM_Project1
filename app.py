a6950e993b27c4f358edd98ceca8c3f594256975
Slyfter <sven.chen@gmx.de>
2023-04-03 16:45:53 +0200

    updated app.py mit Kommentaren und unnötige Codezeilen entfernt

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-import io
 import torch
-import numpy as np
 from flask import Flask, request, send_file
 import matplotlib.pyplot as plt
 from io import BytesIO
@@ -10,36 +8,42 @@ import torchvision.transforms.functional as TF
 
 app = Flask(__name__)
 
-#model_type = "DPT_Hybrid"
+
+# Laden des Modells und auf passende Hardware gesetzt
 model_type = "MiDaS_small"
-
+#model_type = "DPT_Hybrid"
 midas = torch.hub.load("intel-isl/MiDaS", model_type)
-
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 midas.to(device)
 midas.eval()
 
-midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
 
+# Verschiedene Transformationsmethoden für die verschiedenen Modelle
+midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
 if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
     transform = midas_transforms.dpt_transform
 else:
     transform = midas_transforms.small_transform
 
+
+# Hauptseite
 @app.route("/")
 def indexPage():
     return send_file("frontend/index.html")
 
+
+# /predict nimmt eine Bilddatei entgegen, passt die Grösse des Bildes an und gibt die Depthmap als Bild zurück
 @app.route('/predict', methods=['POST'])
 def predict():
+    #Fehlerbehandlung
     try:
-        # Get the uploaded file
+        # Das hochgeladene Bild wird als File-Objekt übergeben
         file = request.files['file']
         
-        # Read image using PIL
+        # Lesen des Bildes mit Pillow
         img = Image.open(file)
 
-        # Resize the image to a larger size
+        # Anpassen der Grösse des Bildes
         img = TF.resize(img, (800, 800))
 
         input_tensor = TF.to_tensor(img)
@@ -57,24 +61,24 @@ def predict():
 
         output = prediction.cpu().numpy()
 
-        # Show result
+        # Anzeigen des Bildes
         fig, ax = plt.subplots()
         ax.imshow(output)
         
-        # Convert plot to Base64 encoded image
+        # Plot als Base64 encoded image zurückgeben
         buffer = BytesIO()
         fig.savefig(buffer, format='png')
         buffer.seek(0)
         image_base64 = base64.b64encode(buffer.getvalue()).decode()
-
-        # Close plot
         plt.close()
 
-        # Return Base64 encoded image
+        # Base64 encoded image als HTML-Tag zurückgeben
         return f'<img src="data:image/png;base64,{image_base64}"/>'
     
+    # Fehlerbehandlung
     except Exception as e:
         return f'Error: {str(e)}'
 
+# Starten des Servers auf dem Port 8000
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
